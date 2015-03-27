aa854d185fe32f8c8bbe6ee24478e06d3dd6c656
alex gian <alexhgian@gmail.com>
2015-03-27 15:48:07 +0900

    First Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rajAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rajAppApp
+ */
+angular.module('rajAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
